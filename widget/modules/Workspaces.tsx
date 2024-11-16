ab8c9b79dddee68391b94bf1577af9f1fe7270ce
willow <42willow@pm.me>
2024-11-16 18:16:21 +1100

    feat: workspaces scrolling

--- widget/modules/Workspaces.tsx
+++ widget/modules/Workspaces.tsx
@@ -4,18 +4,23 @@ import { bind } from "astal";
 export function Workspaces() {
   const hypr = Hyprland.get_default()
 
-  return <box className="Workspaces" vertical={true}>
-      {bind(hypr, "workspaces").as(wss => wss
-          .filter(ws => ws.id > 0) // Filter out negative IDs
-          .sort((a, b) => a.id - b.id)
-          .map(ws => (
-              <button
-                  className={bind(hypr, "focusedWorkspace").as(fw =>
-                      ws === fw ? "focused" : "")}
-                  onClicked={() => ws.focus()}>
-                  {ws.id}
-              </button>
-          ))
-      )}
-  </box>
+  return <eventbox
+    onScroll={(_, e) => {
+        hypr.dispatch("workspace", e.delta_y > 0 ? "+1" : "-1");
+    }}>
+    <box className="Workspaces" vertical={true}>
+        {bind(hypr, "workspaces").as(wss => wss
+            .filter(ws => ws.id > 0) // Filter out negative IDs
+            .sort((a, b) => a.id - b.id)
+            .map(ws => (
+                <button
+                    className={bind(hypr, "focusedWorkspace").as(fw =>
+                        ws === fw ? "focused" : "")}
+                    onClicked={() => ws.focus()}>
+                    {ws.id}
+                </button>
+            ))
+        )}
+    </box>
+  </eventbox>
 }
