36c30a9b9c4cdf8a12d3b942db63dad37475920f
willow <42willow@pm.me>
2025-05-16 17:34:43 +1000

    chore: startup script

--- flake.nix
+++ flake.nix
@@ -4,12 +4,23 @@
     ags.url = "github:aylur/ags";
   };
 
-  outputs = { self, nixpkgs, ags }: let
+  outputs = {
+    self,
+    nixpkgs,
+    ags,
+  }: let
     system = "x86_64-linux";
     pkgs = nixpkgs.legacyPackages.${system};
+    curlew = pkgs.writers.writePython3Bin "cl" {} (builtins.readFile ./curlew.py);
   in {
     devShells.${system}.default = pkgs.mkShell {
+      shellHook = ''
+        echo "hiii :)"
+        echo "you can run the start script using: ${nixpkgs.lib.getName curlew}"
+      '';
+
       buildInputs = [
+        curlew
         # includes astal3 astal4 astal-io by default
         (ags.packages.${system}.default.override {
           extraPackages = [
